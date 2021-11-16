a197c6122114b6c42109d5185e98fbf43ec453d3
vitonman <vitalimaksimov@gmail.com>
2021-11-16 14:40:22 +0200

    studing Angular via Mosh lessons

---
+++ src/app/courses.component.ts
@@ -0,0 +1,30 @@
+import { Component} from "@angular/core";
+import {CoursesService} from "./courses.service";
+
+@Component({
+  selector: 'courses',
+  template: `
+    <h2>{{ "Title: " + getTitle() }}</h2>
+    <ul>
+        <li *ngFor="let course of courses">
+          {{ course }}
+        </li>
+    </ul>`
+})
+
+export class CoursesComponent {
+
+  title = "List of courses";
+  courses;
+
+  constructor(service: CoursesService) {
+    this.courses = service.getCourses();
+  }
+  // Logic for calling an HTTP service
+
+
+  getTitle() {
+    return this.title;
+  }
+
+}
